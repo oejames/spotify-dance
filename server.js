745d0067249118773dc6244fd74848bba1afbd49
Ore James <jamesore200@gmail.com>
2024-01-28 21:40:05 -0800

    Modified playlist view

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.get('/callback', async (req, res) => {
                   src="https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator&theme=0"
                   width="100%"
                   height="100%"
-                  style="min-height: 360px;"
+                  style="min-height: 760px;"
                   frameBorder="0"
                   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                   loading="lazy"
